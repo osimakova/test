fb7dd9bec65b0ce52afcf77c649ac926357cd7aa
Olga Simakov <olgas@Olgas-MBP.hsd1.ca.comcast.net>
2016-01-19 17:33:26 -0800

    Text change

--- day2/helloworld2.java
+++ day2/helloworld2.java
@@ -1,5 +1,5 @@
 public class helloworld2 { 
    public static void main(String[] args) { 
-      System.out.println("Hello, World Day2");
+      System.out.println("Hello, World Day3 New Day");
    }
 }
